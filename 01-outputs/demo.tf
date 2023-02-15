96eee708c9c2aec65519f63e64671165ef2d0604
jaindeepak2008 <jaindeepak2008@gmail.com>
2023-02-15 11:09:55 -0500

    Auto Push

--- 01-outputs/demo.tf
+++ 01-outputs/demo.tf
@@ -1,5 +1,5 @@
-output "smaple" {
-    value = "Hello World"
+output "sample" {
+  value = "Hello World"
 }
 
 variable "demo" {
@@ -17,3 +17,33 @@ output "print-demo" {
 # Note : A variable can be accessed without ${} as well, only if its the only declared entry in the value part of the output. 
         // If you're using the variable in between the strings, you need to enclose them in the quotes and ${} . 
         // There is no concept of single quotes `'` in terraform
+
+
+# This is how we declare a list variable
+variable "example-list" {
+    default = [
+        "AWS",
+        "DevOps",
+        true,
+        100,
+        "SRE"
+    ]
+}
+
+output "ex-list-op" {
+    value = "Welcome to ${var.example-list[0]} and ${var.example-list[1]} Training, Training durations is ${var.example-list[3]} hours , we can also apply for ${var.example-list[4]} jobs"
+}
+
+
+# # This is how we declare a map variable 
+# variable "ex-map" {
+# default = {
+#     Duration = 105, 
+#     Trainer = "Verma", 
+#     Cloud = "AWS"
+#     }
+# }
+
+# output "ex-map-op" {
+#     value = "Welcome to DevOps with ${var.ex-map["Cloud"]} Training and the training duration is ${var.ex-map["Duration"]} , training name is ${var.ex-map["Trainer"]}"
+# }
