e0b58d203b0883ab3c59bd9edc65658d4987bd61
jaindeepak2008 <jaindeepak2008@gmail.com>
2023-02-15 12:47:45 -0500

    Auto Push

--- 03-ec2/ec2.tf
+++ 03-ec2/ec2.tf
@@ -5,5 +5,5 @@ provider "aws" {
 #Creates ec2 instance
 resource "aws_instance" "this" {
     ami                     = "ami-00e87074e52e6c9f9"
-    instance_type           = "t3.micro"
+    instance_type           = "t2.micro"
 }
