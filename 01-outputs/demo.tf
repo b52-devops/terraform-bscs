f97d402ae3c8bc1ad9c42318e5108b53aec1eb6b
jaindeepak2008 <jaindeepak2008@gmail.com>
2023-02-15 11:04:40 -0500

    Auto Push

--- 01-outputs/demo.tf
+++ 01-outputs/demo.tf
@@ -1,3 +1,15 @@
 output "smaple" {
     value = "Hello World"
 }
+
+variable "demo" {
+    default = "Terraform Training"
+}
+
+output "printing-demo" {
+    value = "Value is ${var.demo}"
+}
+
+output "print-demo" {
+    value = var.demo 
+}
