ca22d244bedece9a6e7b574c0a30748ee9fd08d5
jaindeepak2008 <jaindeepak2008@gmail.com>
2023-02-15 13:10:04 -0500

    Auto Push

--- 03-ec2/ec2.tf
+++ 03-ec2/ec2.tf
@@ -12,6 +12,32 @@ resource "aws_instance" "my-ec2" {
     }
 }
 
+#Create security group
+resource "aws_security_group" "allow_ssh" {
+    name        = "allow_ssh"
+    description = "Allow SSH inbound traffic"
+
+    ingress {
+        description      = "SSH from public"
+        from_port        = 22
+        to_port          = 22
+        protocol         = "tcp"
+        cidr_blocks      = ["0.0.0.0/0"]             # [] represents list
+    }
+
+    egress {
+        from_port        = 0
+        to_port          = 0
+        protocol         = "-1"
+        cidr_blocks      = ["0.0.0.0/0"]
+    }
+
+    tags = {
+        Name = "allow_ssh_sg"
+    }
+}
+
+
 #printing the public ip address of the machine
 
 output "public_dns_output" {
