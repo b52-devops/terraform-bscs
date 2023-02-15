360bb603a255c4bd02b282c6d48b135fc950452c
jaindeepak2008 <jaindeepak2008@gmail.com>
2023-02-15 16:55:34 -0500

    Auto Push code

--- 05-modules/main.tf
+++ 05-modules/main.tf
@@ -7,6 +7,6 @@ module "sg" {
     source = "./sg"
 }
 
-output "server_public_ip" {
-    value = aws_instance.my-ec2.public_ip
-}
+# output "server_public_ip" {
+#     value = aws_instance.my-ec2.public_ip
+# }
