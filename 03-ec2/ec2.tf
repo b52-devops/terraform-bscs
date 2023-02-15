1b296fb2ef4c9093eea994bd29252a2a4ebacda8
jaindeepak2008 <jaindeepak2008@gmail.com>
2023-02-15 13:13:53 -0500

    Auto Push

--- 03-ec2/ec2.tf
+++ 03-ec2/ec2.tf
@@ -6,6 +6,7 @@ provider "aws" {
 resource "aws_instance" "my-ec2" {
     ami                     = "ami-00e87074e52e6c9f9"
     instance_type           = "t2.micro"
+    vpc_security_group_ids  = [aws_security_group.allow_ssh.id]
 
     tags = {
         Name = "terraform-server"
