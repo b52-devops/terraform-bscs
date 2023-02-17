b4ae1e8b5436ab5da3b75853b39692477db904e7
jaindeepak2008 <jaindeepak2008@gmail.com>
2023-02-17 12:19:03 -0500

    Auto push code

--- robot-project/ec2/data.tf
+++ robot-project/ec2/data.tf
@@ -1,6 +1,6 @@
 #Declaring the data source  
 data "aws_ami" "lab-image" {
 most_recent      = true
-name_regex       = "b52-ansible-dev-23jan23"
+name_regex       = "b52-ansible-dev-17feb2023-dj"
 owners           = ["self"]
 }
