81570f583432bf9fa36e589e0075e80be016730d
jaindeepak2008 <jaindeepak2008@gmail.com>
2023-02-23 13:19:47 -0500

    Auto push code

--- robot-project/ec2/sg.tf
+++ robot-project/ec2/sg.tf
@@ -4,21 +4,21 @@ resource "aws_security_group" "allow_all" {
     name        = "allow_${var.COMPONENT}"
     description = "Allow SSH inbound traffic"
 
-ingress {
-    description      = "HTTP from INTERNET"
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    }
-
-ingress {
-        description      = "SSH from public"
+    ingress {
+        description      = "HTTP from INTERNET"
         from_port        = 0
         to_port          = 0
         protocol         = "-1"
-        cidr_blocks      = ["0.0.0.0/0"]             # [] represents list
-    }
+        cidr_blocks      = ["0.0.0.0/0"]
+        }
+
+    ingress {
+            description      = "SSH from public"
+            from_port        = 0
+            to_port          = 65535
+            protocol         = "tcp"
+            cidr_blocks      = ["0.0.0.0/0"]             # [] represents list
+        }
 
     egress {
         from_port        = 0
