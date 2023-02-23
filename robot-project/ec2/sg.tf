661307cfc3af786172768d487e043e332b924cdf
jaindeepak2008 <jaindeepak2008@gmail.com>
2023-02-23 11:29:41 -0500

    Auto push code

--- robot-project/ec2/sg.tf
+++ robot-project/ec2/sg.tf
@@ -11,11 +11,12 @@ ingress {
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
     }
+
 ingress {
         description      = "SSH from public"
         from_port        = 0
         to_port          = 0
-        protocol         = "tcp"
+        protocol         = "-1"
         cidr_blocks      = ["0.0.0.0/0"]             # [] represents list
     }
 
