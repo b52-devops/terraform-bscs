8729e5eaaa7c88bd76f405512ea1053d349dcba0
jaindeepak2008 <jaindeepak2008@gmail.com>
2023-02-24 10:37:54 -0500

    auto push

--- robot-project/ec2/main.tf
+++ robot-project/ec2/main.tf
@@ -39,7 +39,7 @@ resource "aws_instance" "my-ec2" {
         type     = "ssh"
         user     = "centos"
         password = "DevOps321"
-        host     = self.public_ip         # self. will only work if it's inside the resource ; If not, we need to use aws_instance.my-ec2.public_ip
+        host     = self.private_ip         # self. will only work if it's inside the resource ; If not, we need to use aws_instance.my-ec2.public_ip
         }
 
         inline = [
