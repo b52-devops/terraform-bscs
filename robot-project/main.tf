7eb5030307811644424585c17c7add98c64e9eab
jaindeepak2008 <jaindeepak2008@gmail.com>
2023-02-28 10:02:45 -0500

    auto push

--- robot-project/main.tf
+++ robot-project/main.tf
@@ -1,56 +1,56 @@
-module "mongodb" {
-    source      = "./ec2"
-    COMPONENT   = "mongodb"
-    APP_VERSION = "null"
-}
+# module "mongodb" {
+#     source      = "./ec2"
+#     COMPONENT   = "mongodb"
+#     APP_VERSION = "null"
+# }
 
-module "catalogue" {
-    source      = "./ec2"
-    COMPONENT   = "catalogue"
-    APP_VERSION = "0.0.8"
-}
+# module "catalogue" {
+#     source      = "./ec2"
+#     COMPONENT   = "catalogue"
+#     APP_VERSION = "0.0.8"
+# }
 
-module "redis" {
-    source      = "./ec2"
-    COMPONENT   = "redis"
-    APP_VERSION = "null"
-}
+# module "redis" {
+#     source      = "./ec2"
+#     COMPONENT   = "redis"
+#     APP_VERSION = "null"
+# }
 
-module "user" {
-    source      = "./ec2"
-    COMPONENT   = "user"
-    APP_VERSION = "0.0.9"
-}
+# module "user" {
+#     source      = "./ec2"
+#     COMPONENT   = "user"
+#     APP_VERSION = "0.0.9"
+# }
 
-module "cart" {
-    source      = "./ec2"
-    COMPONENT   = "cart"
-    APP_VERSION = "0.0.8"
-}
+# module "cart" {
+#     source      = "./ec2"
+#     COMPONENT   = "cart"
+#     APP_VERSION = "0.0.8"
+# }
 
-module "rabbitmq" {
-    source      = "./ec2"
-    COMPONENT   = "rabbitmq"
-    APP_VERSION = "null"
-}
+# module "rabbitmq" {
+#     source      = "./ec2"
+#     COMPONENT   = "rabbitmq"
+#     APP_VERSION = "null"
+# }
 
-module "payment" {
-    source      = "./ec2"
-    COMPONENT   = "payment"
-    APP_VERSION = "0.0.2"
-}
+# module "payment" {
+#     source      = "./ec2"
+#     COMPONENT   = "payment"
+#     APP_VERSION = "0.0.2"
+# }
 
-module "shipping" {
-    source      = "./ec2"
-    COMPONENT   = "shipping"
-    APP_VERSION = "0.0.4"
-}
+# module "shipping" {
+#     source      = "./ec2"
+#     COMPONENT   = "shipping"
+#     APP_VERSION = "0.0.4"
+# }
 
-module "mysql" {
-    source      = "./ec2"
-    COMPONENT   = "mysql"
-    APP_VERSION = "null"
-}
+# module "mysql" {
+#     source      = "./ec2"
+#     COMPONENT   = "mysql"
+#     APP_VERSION = "null"
+# }
 
 module "frontend" {
     source      = "./ec2"
