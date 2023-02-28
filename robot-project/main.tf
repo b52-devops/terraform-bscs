8b63a06990809c48cc99ff039442b8ce5c65e9ab
jaindeepak2008 <jaindeepak2008@gmail.com>
2023-02-28 10:44:58 -0500

    Auto push

--- robot-project/main.tf
+++ robot-project/main.tf
@@ -1,59 +1,67 @@
-module "mongodb" {
+module "component" {
+    for_each = var.COMPONENTS
     source      = "./ec2"
-    COMPONENT   = "mongodb"
-    APP_VERSION = "null"
+    COMPONENT = each.key
+    APP_VERSION = each.value.APP_VERSION
 }
 
-module "catalogue" {
-    source      = "./ec2"
-    COMPONENT   = "catalogue"
-    APP_VERSION = "0.0.8"
-}
 
-module "redis" {
-    source      = "./ec2"
-    COMPONENT   = "redis"
-    APP_VERSION = "null"
-}
+# module "mongodb" {
+#     source      = "./ec2"
+#     COMPONENT   = "mongodb"
+#     APP_VERSION = "null"
+# }
 
-module "user" {
-    source      = "./ec2"
-    COMPONENT   = "user"
-    APP_VERSION = "0.0.9"
-}
+# module "catalogue" {
+#     source      = "./ec2"
+#     COMPONENT   = "catalogue"
+#     APP_VERSION = "0.0.8"
+# }
 
-module "cart" {
-    source      = "./ec2"
-    COMPONENT   = "cart"
-    APP_VERSION = "0.0.8"
-}
+# module "redis" {
+#     source      = "./ec2"
+#     COMPONENT   = "redis"
+#     APP_VERSION = "null"
+# }
 
-module "rabbitmq" {
-    source      = "./ec2"
-    COMPONENT   = "rabbitmq"
-    APP_VERSION = "null"
-}
+# module "user" {
+#     source      = "./ec2"
+#     COMPONENT   = "user"
+#     APP_VERSION = "0.0.9"
+# }
 
-module "payment" {
-    source      = "./ec2"
-    COMPONENT   = "payment"
-    APP_VERSION = "0.0.2"
-}
+# module "cart" {
+#     source      = "./ec2"
+#     COMPONENT   = "cart"
+#     APP_VERSION = "0.0.8"
+# }
 
-module "shipping" {
-    source      = "./ec2"
-    COMPONENT   = "shipping"
-    APP_VERSION = "0.0.4"
-}
+# module "rabbitmq" {
+#     source      = "./ec2"
+#     COMPONENT   = "rabbitmq"
+#     APP_VERSION = "null"
+# }
 
-module "mysql" {
-    source      = "./ec2"
-    COMPONENT   = "mysql"
-    APP_VERSION = "null"
-}
+# module "payment" {
+#     source      = "./ec2"
+#     COMPONENT   = "payment"
+#     APP_VERSION = "0.0.2"
+# }
 
-module "frontend" {
-    source      = "./ec2"
-    COMPONENT   = "frontend"
-    APP_VERSION = "0.0.1"
-}
+# module "shipping" {
+#     source      = "./ec2"
+#     COMPONENT   = "shipping"
+#     APP_VERSION = "0.0.4"
+# }
+
+# module "mysql" {
+#     source      = "./ec2"
+#     COMPONENT   = "mysql"
+#     APP_VERSION = "null"
+# }
+
+# module "frontend" {
+#     source      = "./ec2"
+#     COMPONENT   = "frontend"
+#     APP_VERSION = "0.0.1"
+# }
---
+++ robot-project/vars.tf
@@ -0,0 +1,35 @@
+#map variable 
+variable "COMPONENTS" {
+    default = {
+        mongodb = {
+            APP_VERSION = "null"
+        },
+        catalogue = {
+            APP_VERSION = "0.0.8"
+        },
+        redis = {
+            APP_VERSION = "null"
+        },
+        user = {
+            APP_VERSION = "0.0.9"
+        },
+        cart = {
+            APP_VERSION = "0.0.8"
+        },
+        rabbitmq = {
+            APP_VERSION = "null"
+        },
+        payment = {
+            APP_VERSION = "0.0.2"
+        },
+        shipping = {
+            APP_VERSION = "0.0.4"
+        },
+        mysql = {
+            APP_VERSION = "null"
+        },
+        frontend = {
+            APP_VERSION = "0.0.1"
+        },
+    }
+}
