7b3393844fd3427f62b967bbb691a3859b90c779
jaindeepak2008 <jaindeepak2008@gmail.com>
2023-02-28 10:51:05 -0500

    Auto push

--- robot-project/ec2/route53.tf
+++ robot-project/ec2/route53.tf
@@ -2,6 +2,6 @@ resource "aws_route53_record" "component" {
     zone_id = "Z0112540UNPLCQ33VGRQ"
     name    = "${var.COMPONENT}-dev.robosho.internal"
     type    = "A"
-    ttl     = 300
+    ttl     = 10
     records = [aws_instance.my-ec2.private_ip]
 }
