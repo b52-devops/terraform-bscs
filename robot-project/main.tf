7193d0d215a1ee28337cefe83701ebc874a7953b
jaindeepak2008 <jaindeepak2008@gmail.com>
2023-02-28 09:45:52 -0500

    auto push

--- robot-project/main.tf
+++ robot-project/main.tf
@@ -3,3 +3,57 @@ module "frontend" {
     COMPONENT   = "frontend"
     APP_VERSION = "0.0.1"
 }
+
+module "frontend" {
+    source      = "./ec2"
+    COMPONENT   = "mongodb"
+    APP_VERSION = "null"
+}
+
+module "catalogue" {
+    source      = "./ec2"
+    COMPONENT   = "catalogue"
+    APP_VERSION = "0.0.8"
+}
+
+module "redis" {
+    source      = "./ec2"
+    COMPONENT   = "redis"
+    APP_VERSION = "null"
+}
+
+module "user" {
+    source      = "./ec2"
+    COMPONENT   = "user"
+    APP_VERSION = "0.0.9"
+}
+
+module "cart" {
+    source      = "./ec2"
+    COMPONENT   = "cart"
+    APP_VERSION = "0.0.8"
+}
+
+module "rabbitmq" {
+    source      = "./ec2"
+    COMPONENT   = "rabbitmq"
+    APP_VERSION = "null"
+}
+
+module "payment" {
+    source      = "./ec2"
+    COMPONENT   = "payment"
+    APP_VERSION = "0.0.2"
+}
+
+module "shipping" {
+    source      = "./ec2"
+    COMPONENT   = "shipping"
+    APP_VERSION = "0.0.4"
+}
+
+module "mysql" {
+    source      = "./ec2"
+    COMPONENT   = "mysql"
+    APP_VERSION = "null"
+}
